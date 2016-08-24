06ad672a0344f01ecc3e5374e1a4c40ba8549bcb
kanxinwei <shink_7770@qq.com>
2016-08-24 14:29:38 +0800

    增加获取用户列表的方法

--- lib/dingtalk/user.rb
+++ lib/dingtalk/user.rb
@@ -1,15 +1,13 @@
 ﻿module DingTalk
   class User
+    def self.list;Department.list.flat_map{|d| d.users} end
     def self.get(id, need_pretension = false)
       if need_pretension
-        @uid_user_hash ||= pretension
+        @uid_user_hash ||= list.map{|u| [u.id, u]}.to_h
         @uid_user_hash[id]
       else
         new(Service.user_get(userid: id))
       end
-    end
-    def self.pretension
-      Department.list.flat_map{|d| d.users}.map{|u| [u.id, u]}.to_h
     end
     attr_reader :id, :name
     def initialize(hash)
